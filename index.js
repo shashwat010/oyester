cccd3ff4f22781957bed8bf9ecbac25056ae76ac
shashwat010 <76944311+shashwat010@users.noreply.github.com>
2022-10-09 19:45:41 +0530

    adding start cmd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 connectToMongo();
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
